f826730fea350cfe870c6b54e75b81abcf40f77d
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-06-03 21:32:46 +0530

    Create logical.js

---
+++ Java Script/Operators/logical.js
@@ -0,0 +1,11 @@
+let user = "vanshu";
+let isLoggesIn = true;
+let isPaid = false;
+
+if(isLoggedIn && isPaid){
+    console.log("hello vanshu . How are yoU?");
+}
+else{
+    console.log("i am good");
+    
+}
